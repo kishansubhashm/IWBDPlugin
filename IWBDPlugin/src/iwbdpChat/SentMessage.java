cb2fdb7c5a4599d192c5ea874204787223337c2f
KishanSubhashMiryala <KishanSubhashMiryala@kishan-pc>
2014-04-01 19:47:15 +0530

    second commit

---
+++ IWBDPlugin/src/iwbdpChat/SentMessage.java
@@ -0,0 +1,47 @@
+package iwbdpChat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SentMessage
+ */
+@WebServlet("/SentMessage")
+public class SentMessage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SentMessage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				//out.print(request.getParameter("ed")+request.getParameter("wrd"));
+				System.out.print("hi");
+				System.out.println("entered message"+request.getParameter("ed"));
+				out.print("\n"+request.getParameter("ed"));
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ IWBDPlugin/src/iwbdpChat/TestMessage.java
@@ -0,0 +1,54 @@
+package iwbdpChat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestMessage
+ */
+@WebServlet("/TestMessage")
+public class TestMessage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestMessage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				response.setContentType("text/plain");
+				 response.setCharacterEncoding("UTF-8"); 
+				PrintWriter out = response.getWriter();
+				System.out.println("in retrieve messages");
+//				Scanner sc=new Scanner(System.in);
+				String word="kishan";
+//				System.out.println(request.getParameter("title"));
+//				System.out.println("from RetrieveMessages servlet");
+//				System.out.print("enter any word :");
+//				word=sc.next();
+//				System.out.println("entered word is :"+word);
+				out.println("\n"+word);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
